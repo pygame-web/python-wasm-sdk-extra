6bde77c9c094ddf749d97ba3196da56271bef22d
pmp-p <mail.peny@free.fr>
2024-08-27 13:44:51 +0200

    increamental file

--- build.sh
+++ build.sh
@@ -1,28 +1,9 @@
 #!/bin/bash
 
-. /etc/lsb-release
-export DISTRIB="${DISTRIB_ID}-${DISTRIB_RELEASE}"
-export SDKROOT=${SDKROOT:-/opt/python-wasm-sdk}
-export CIVER=${CIVER:-$DISTRIB}
+if cd /opt/python-wasm-sdk
+then
+    find . -type f > sdk.base
+    . scripts/emsdk-extra.sh
+fi
 
-TAG=emsdk
 
-echo "making sdk-extra $TAG tarball" 1>&2
-
-pushd /
-mkdir -p /tmp/sdk
-tar -cpPR \
-    ${SDKROOT}/config \
-    ${SDKROOT}/python3-was? \
-    ${SDKROOT}/wasm32-*-shell.sh \
-    ${SDKROOT}/*sdk \
-    ${SDKROOT}/scripts/*sdk-fetch.sh \
-    ${SDKROOT}/devices/* \
-    ${SDKROOT}/prebuilt/* \
-     > /tmp/sdk/python${PYBUILD}-${TAG}-sdk-extra-${CIVER}.tar
-
-    lz4 -c --favor-decSpeed --best /tmp/sdk/python${PYBUILD}-${TAG}-sdk-extra-${CIVER}.tar \
-     > /tmp/sdk/python${PYBUILD}-${TAG}-sdk-extra-${CIVER}.tar.lz4
-
-echo "done"  1>&2
-popd
---
+++ pack.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+. /etc/lsb-release
+export DISTRIB="${DISTRIB_ID}-${DISTRIB_RELEASE}"
+export SDKROOT=${SDKROOT:-/opt/python-wasm-sdk}
+export CIVER=${CIVER:-$DISTRIB}
+
+TAG=emsdk
+
+pushd /opt/python-wasm-sdk
+    echo "Removing $(wc -l sdk.base) files belonging to SDK"
+    for sdkfile in $(cat sdk.base)
+    do
+        rm $f
+    done
+popd
+
+echo "Making sdk-extra $TAG tarball" 1>&2
+
+
+pushd /
+mkdir -p /tmp/sdk
+tar -cpPR \
+    ${SDKROOT}/config \
+    ${SDKROOT}/python3-was? \
+    ${SDKROOT}/wasm32-*-shell.sh \
+    ${SDKROOT}/*sdk \
+    ${SDKROOT}/scripts/*sdk-fetch.sh \
+    ${SDKROOT}/devices/* \
+    ${SDKROOT}/prebuilt/* \
+     > /tmp/sdk/python${PYBUILD}-${TAG}-sdk-extra-${CIVER}.tar
+
+    lz4 -c --favor-decSpeed --best /tmp/sdk/python${PYBUILD}-${TAG}-sdk-extra-${CIVER}.tar \
+     > /tmp/sdk/python${PYBUILD}-${TAG}-sdk-extra-${CIVER}.tar.lz4
+
+    du -hs /tmp/sdk/*
+
+    rm /tmp/sdk/python${PYBUILD}-${TAG}-sdk-extra-${CIVER}.tar
+echo "done"  1>&2
+popd
